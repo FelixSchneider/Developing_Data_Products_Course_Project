2dc964436705f499c2143f9899e8908aa0a662d2
Felix Schneider <felix.schneider@arcor.de>
2020-08-18 09:49:38 +0200

    added geom_abline() with slope and intercept

--- CourseProjectApp/server.R
+++ CourseProjectApp/server.R
@@ -7,14 +7,16 @@ shinyServer(function(input, output) {
         fr <- input$formulaRegressor
         regressors <- ifelse(length(fr)==0,".",paste(fr,collapse="+"))
         fml <- paste("Fertility ~ ",regressors)
-        # fit <- lm(formula(fml),swiss)
         fit <- lm(fml,swiss)
         list(formula=fml,coef=summary(fit)$coefficients)
     })
     
     output$plot1 <- renderPlot({
-        ggplot(swiss, aes(.data[[input$plotRegressor]], Fertility)) + geom_point() #+
-            # geom_abline(slope=model()$fit$coef[2], intercept=model()$fit$coef[1])
+        pr <- input$plotRegressor
+        ggplot(swiss, aes(.data[[pr]], Fertility)) + geom_point() +
+            # geom_abline(slope=model()$coef["Agriculture","Estimate"],
+            geom_abline(slope=model()$coef[{{pr}},"Estimate"],
+                        intercept=model()$coef["(Intercept)","Estimate"])
     })
     
     output$formula1 <- renderText(
